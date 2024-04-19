d0730a1ccbe76b7074df04189b5e48658491ca9a
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-19 16:45:56 +0300

    migrate

--- users/migrations/0002_alter_user_options_remove_user_username_user_avatar_and_more.py
+++ users/migrations/0002_alter_user_options_remove_user_username_user_avatar_and_more.py
@@ -6,36 +6,45 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='user',
-            options={'verbose_name': 'Пользователь', 'verbose_name_plural': 'Пользователи'},
+            name="user",
+            options={
+                "verbose_name": "Пользователь",
+                "verbose_name_plural": "Пользователи",
+            },
         ),
         migrations.RemoveField(
-            model_name='user',
-            name='username',
+            model_name="user",
+            name="username",
         ),
         migrations.AddField(
-            model_name='user',
-            name='avatar',
-            field=models.ImageField(blank=True, null=True, upload_to='users/avatar/', verbose_name='аватар'),
+            model_name="user",
+            name="avatar",
+            field=models.ImageField(
+                blank=True, null=True, upload_to="users/avatar/", verbose_name="аватар"
+            ),
         ),
         migrations.AddField(
-            model_name='user',
-            name='city',
-            field=models.CharField(blank=True, max_length=35, null=True, verbose_name='страна'),
+            model_name="user",
+            name="city",
+            field=models.CharField(
+                blank=True, max_length=35, null=True, verbose_name="страна"
+            ),
         ),
         migrations.AddField(
-            model_name='user',
-            name='phone',
-            field=models.CharField(blank=True, max_length=35, null=True, verbose_name='номер телефона'),
+            model_name="user",
+            name="phone",
+            field=models.CharField(
+                blank=True, max_length=35, null=True, verbose_name="номер телефона"
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='email',
-            field=models.EmailField(max_length=254, unique=True, verbose_name='Email'),
+            model_name="user",
+            name="email",
+            field=models.EmailField(max_length=254, unique=True, verbose_name="Email"),
         ),
     ]
--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-NULLABLE = {'blank': True, 'null': True}
+NULLABLE = {"blank": True, "null": True}
 
 
 # Create your models here.
@@ -10,11 +10,23 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True, verbose_name="Email")
 
-    phone = models.CharField(max_length=35, verbose_name="номер телефона", **NULLABLE)
-    city = models.CharField(max_length=35, verbose_name="страна", **NULLABLE)
-    avatar = models.ImageField(upload_to="users/avatar/", verbose_name="аватар", **NULLABLE)
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Номер телефона",
+        help_text="Укажите телефон",
+        **NULLABLE
+    )
+    city = models.CharField(
+        max_length=35, verbose_name="Страна", help_text="Укажите город", **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar/",
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+        **NULLABLE
+    )
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
