390d3b3fa546f27e8287e358f450deeb7230fbd6
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-20 00:27:41 +0300

    run black and install isort and run

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import CourseViewSet, LessonListApiView, LessonRetrieveApiView, LessonCreateApiView, LessonUpdateApiView, LessonDestroyApiView
+from materials.views import (CourseViewSet, LessonCreateApiView,
+                             LessonDestroyApiView, LessonListApiView,
+                             LessonRetrieveApiView, LessonUpdateApiView)
 
 app_name = MaterialsConfig.name
 
@@ -13,8 +15,14 @@ urlpatterns = [
     path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
     path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
     path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
-    path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
-    path("lessons/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lessons_delete"),
+    path(
+        "lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"
+    ),
+    path(
+        "lessons/<int:pk>/delete/",
+        LessonDestroyApiView.as_view(),
+        name="lessons_delete",
+    ),
 ]
 
 urlpatterns += router.urls
