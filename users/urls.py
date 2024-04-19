e1ee3a5b00b9cf5ce616839fb9e78fc52efe54a8
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-20 00:42:09 +0300

    add generic for edit user

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import UserUpdateApiView
+
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path(
+        "users/<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"
+    ),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import UpdateAPIView
+
+from users.models import User
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class UserUpdateApiView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
