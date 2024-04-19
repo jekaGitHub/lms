05e2389a01bf03cf3cb72b6d9b79942507ef8842
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-19 09:49:56 +0300

    create model for user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Email")
+
+    phone = models.CharField(max_length=35, verbose_name="номер телефона", **NULLABLE)
+    city = models.CharField(max_length=35, verbose_name="страна", **NULLABLE)
+    avatar = models.ImageField(upload_to="users/avatar/", verbose_name="аватар", **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
