4f9dc13ec1f6889010367d9efd4372f7fd8b166e
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-19 18:11:51 +0300

    create models and migrate

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
+from users.models import NULLABLE
+
+
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    image = models.ImageField(
+        upload_to="preview/course",
+        verbose_name="Превью",
+        help_text="Загрузите фото",
+        **NULLABLE,
+    )
+    description = models.TextField(
+        verbose_name="Описание курса", help_text="Укажите описание курса", **NULLABLE
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=150,
+        help_text="Укажите название урока",
+        verbose_name="Название урока",
+    )
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        related_name="lessons",
+        help_text="Выберите курс",
+        verbose_name="Курс",
+        **NULLABLE,
+    )
+
+    description = models.TextField(
+        verbose_name="Описание урока", help_text="Укажите описание урока", **NULLABLE
+    )
+    image = models.ImageField(
+        upload_to="preview/lesson",
+        verbose_name="Превью",
+        help_text="Загрузите фото",
+        **NULLABLE,
+    )
+    url = models.URLField(
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео урока",
+        **NULLABLE,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
