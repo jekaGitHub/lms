0e216c5d21e84c6615faf0b298dfba9eafbe9c2e
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-20 19:09:20 +0300

    create csu.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from users.models import User
+
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "password", "phone", "city",)
+    list_filter = ("email", "city")
+    search_fields = ("email", "phone")
