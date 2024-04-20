38099b088e8ea41346f99f86950fadf5b2ea9f0e
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-20 23:01:41 +0300

    edit url

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from users.views import UserUpdateApiView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("users/<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"),
+    path("<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"),
 ]
