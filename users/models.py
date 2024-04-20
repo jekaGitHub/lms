162077e50005239c889426b8f2574c85fcc4b0e9
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-20 18:31:53 +0300

    create model Payments

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-NULLABLE = {"blank": True, "null": True}
+from materials.models import NULLABLE, Course, Lesson
 
 
 # Create your models here.
@@ -32,3 +32,48 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payments(models.Model):
+    PAYMENT_METHOD = (
+        ("cash", "наличные"),
+        ("transfer", "перевод на счет"),
+    )
+
+    user_payer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="payments",
+        verbose_name="Плательщик",
+        help_text="Укажите плательщика",
+    )
+    payment_date = models.DateTimeField(
+        verbose_name="Дата оплаты", help_text="Укажите дату оплаты"
+    )
+    course_paid = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="payments_course",
+        verbose_name="Оплаченный курс",
+        help_text="Укажите оплаченный курс",
+    )
+    lesson_paid = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        related_name="payments_lesson",
+        verbose_name="Оплаченный урок",
+        help_text="Укажите оплаченный урок",
+    )
+    payment_amount = models.DecimalField(
+        decimal_places=2,
+        max_digits=7,
+        verbose_name="Сумма оплаты",
+        help_text="Укажите сумму оплаты",
+    )
+    payment_method = models.CharField(
+        choices=PAYMENT_METHOD,
+        default="daily",
+        verbose_name="Способ оплаты",
+        help_text="Выберите способ оплаты",
+        **NULLABLE
+    )
--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,8 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import UserUpdateApiView
 
-
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path(
-        "users/<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"
-    ),
+    path("users/<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"),
 ]
