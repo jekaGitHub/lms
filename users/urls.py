5e4c3c8609c84e5e58f08feb76364c84a3ccd531
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-22 23:38:14 +0300

    add code

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserUpdateApiView
+from users.views import UserUpdateApiView, PaymentsListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"),
+    path("<int:pk>/update/", UserUpdateApiView.as_view(), name="users-update"),
+
+    # payments
+    path("payments/", PaymentsListAPIView.as_view(), name="payments-list"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import UpdateAPIView, ListAPIView
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 
 
 # Create your views here.
 class UserUpdateApiView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentsListAPIView(ListAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
