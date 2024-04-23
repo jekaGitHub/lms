8dc9e107dbf0ebd19e49dbeb61a13ccc93b36176
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-23 22:43:03 +0300

    add filters and ordering

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import UpdateAPIView, ListAPIView
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from users.models import User, Payments
 from users.serializers import UserSerializer, PaymentsSerializer
@@ -13,3 +15,6 @@ class UserUpdateApiView(UpdateAPIView):
 class PaymentsListAPIView(ListAPIView):
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ('course_paid', 'lesson_paid', 'payment_method')
+    ordering_fields = ('payment_date',)
