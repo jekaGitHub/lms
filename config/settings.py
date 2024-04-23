e269bed3a5a1b35851ca9aa2eed486f83ac475a5
Evgeny Safonov <jeka-luda@yandex.ru>
2024-04-23 21:53:39 +0300

    install django-filter

--- config/settings.py
+++ config/settings.py
@@ -43,9 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "rest_framework",
     "users",
     "materials",
+    "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
