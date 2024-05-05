25fb16b74616a077d0a2451105f4908e262f2ed5
Evgeny Safonov <jeka-luda@yandex.ru>
2024-05-05 23:39:36 +0300

    task 4

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from materials.models import Course
+from users.models import User
+
 
 # Create your tests here.
+class CourseTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email='user@example.com')
+
+        self.course = Course.objects.create(name="Тест", description="Новое тестовое описание", owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_course_retrieve(self):
+        url = reverse("materials:course-detail", args=(self.course.pk,))
+        response = self.client.get(url)
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+
+        self.assertEqual(
+            data.get("name"), self.course.name
+        )
+
+    def test_course_create(self):
+        url = reverse("materials:course-list")
+        data = {
+            "name": "Экспериментальный"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 2
+        )
