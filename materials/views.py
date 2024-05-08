c167a27743e16a2e92e9ab3174d6a91624b69340
Evgeny Safonov <jeka-luda@yandex.ru>
2024-05-08 23:26:35 +0300

    correct tests.py

--- materials/views.py
+++ materials/views.py
@@ -64,4 +64,4 @@ class LessonUpdateApiView(UpdateAPIView):
 class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    permission_classes = (IsAuthenticated, ~IsModer, IsOwner)
+    permission_classes = (IsAuthenticated, IsOwner)
